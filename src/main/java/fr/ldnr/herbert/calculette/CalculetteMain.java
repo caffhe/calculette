0d9e628b891cfc9f8d645c7989e9631a1cb665ad
Herbert Caffarel <herbert.caffarel@cicef.pro>
2023-02-01 09:46:34 +0100

    First commit

---
+++ src/main/java/fr/ldnr/herbert/calculette/CalculetteMain.java
@@ -0,0 +1,15 @@
+/*
+ */
+
+package fr.ldnr.herbert.calculette;
+
+/**
+ *
+ * @author Herbert Caffarel
+ */
+public class CalculetteMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
